852a1e42bc0fb24ae27f3ac68e081678849653cd
GwendiWisnuAnggoro <105532618+GwendiWisnuAnggoro@users.noreply.github.com>
2023-08-13 11:14:56 +0700

    Update frmk_v1.0_forGwendi.js

--- js/frmk_v1.0_forGwendi.js
+++ js/frmk_v1.0_forGwendi.js
@@ -62,3 +62,57 @@ function panggilSemuaQuery(name){
 function tambahEvent(nama, acara, fungsi){
 	return nama.addEventListener(acara, fungsi);
 }
+
+
+class AmbilId {
+	constructor(link){
+		this.result = this.Ambil(link)
+	}
+
+	Ambil(link){
+		const regex = /\/d\/([a-zA-Z0-9-_]+)/;
+		const match = link.match(regex);
+		if (match && match[1]) {
+			return match[1];
+		} else {
+			return null;
+		}
+	}
+}
+
+
+class AmbilData {
+    constructor(sheetId, nameRows){
+        this.result;
+
+        setInterval(()=>{
+            this.GetData(sheetId.toString(), nameRows.toString())
+
+        },500)
+
+    }
+
+    GetData(id, nama){
+        const url = `https://docs.google.com/spreadsheets/d/${id}/gviz/tq?tqx=out:json&tq=SELECT ${nama}`
+        fetch(url)
+        .then(res => res.text())
+        .then(result => {
+            const json = JSON.parse(result.substr(47).slice(0, -2));
+            const rows = json.table.rows;
+            const data = [];
+
+            rows.forEach((e, i) => {
+                const dataObject = {
+                    value: e.c[0].v,
+                    rows: (i+1),
+                }
+                data.push(dataObject)
+            });
+
+            this.result = data;
+        })
+        .catch(err =>{
+            console.error(err)
+        })
+    }
+}
